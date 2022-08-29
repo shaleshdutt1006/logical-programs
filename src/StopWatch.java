8863de8fd1e707fa64f426cef8370d05c6b1effc
shalesh1006 <shaleshdutt1006@gmail.com>
2022-08-29 19:05:56 +0530

    Refactored : Adding Stop Watch Program

---
+++ src/StopWatch.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class StopWatch {
+
+	public long startTimer = 0;
+	public long stopTimer = 0;
+	public long elapsed;
+
+	// to start timer
+	public void start() {
+		startTimer = System.currentTimeMillis();
+		System.out.println("Start Time is: " + startTimer);
+	}
+
+	// to stop timer
+	public void stop() {
+		stopTimer = System.currentTimeMillis();
+		System.out.println("Stop Time is: " + stopTimer);
+	}
+
+	public long getElapsedTime() {
+		elapsed = stopTimer - startTimer;
+		return elapsed;
+	}
+
+	public static void main(String[] args) throws Exception {
+		Scanner u = new Scanner(System.in);
+
+		StopWatch sw = new StopWatch();
+
+		System.out.println("Press '1' to Start Time: ");
+		u.nextInt();
+		sw.start();
+
+		System.out.println();
+		System.out.println("Press '2' to Stop Time: ");
+		u.nextInt();
+		sw.stop();
+
+		long l = sw.getElapsedTime();
+		System.out.println();
+		System.out.println("Total Time Elapsed(in millisec) is:" + l);
+		System.out.println();
+		System.out.println("Converting millisec to seconds: " + (l / 1000) + " sec");
+	}
+}
